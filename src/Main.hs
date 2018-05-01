e617e515f9bbae8c328a4d1af9a571d7142a62a7
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-05-01 10:55:22 +0200

    improvements

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,19 @@ module Main where
 import           Data.Ini
 import           System.Environment
 import           Ast
+import System.Exit
 
 
+exit    = exitWith ExitSuccess
 
-main :: IO ()
 main = do
+     args <- getArgs
+     case args of
+        ["-h"] -> putStr "Subs - more than snippets, less than a transpiler" >> exit
+        ["-v"] -> putStr "Subs - Version 0.1" >> exit
+        otherwise -> run
+
+run = do
   input <- getContents
   configFile <- getConfigPath
   iniFile <- readIniFile configFile
