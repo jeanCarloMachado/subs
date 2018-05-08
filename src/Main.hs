cd462abcbf2d0f2574d901f9b9692baf6a75812f
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-05-08 07:57:33 +0200

    improvements

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ import           Data.List
 import           Data.Ini (lookupValue, sections, keys, readIniFile)
 import           System.Environment
 import           Ast
-import           Data.Text (unpack)
+import           Data.Text (unpack, pack)
 import System.Exit
 import           Data.String.Utils
 
@@ -15,36 +15,42 @@ helpStr = "Subs - more than snippets, less than a transpiler\n\
 \    Usage:\n\
 \    \n\
 \    subs <<< 'snippet'                 parse the snippet\n\
+\    subs -p                            set prefered section to get value\n\
 \    subs -v                            get version\n\
 \    subs -k                            get all keys on the ini file"
 
 main = do
      args <- getArgs
-     case args of
-        ["-h"] -> putStr helpStr  >> exit
-        ["-v"] -> putStr "Subs - Version 0.1" >> exit
-        ["-k"] -> showKeys >> exit
-        otherwise -> run
+     let firstArg = head args
+     case firstArg of
+        "-h" -> putStr helpStr  >> exit
+        "-v" -> putStr "Subs - Version 0.1" >> exit
+        "-k" -> showKeys >> exit
+        "-p" -> run (last args)
+        otherwise -> run ""
 
 showKeys = do
   ini <- parseConfig
   let iniSections = sections ini
       keysText = map (\x -> Data.Ini.keys x ini) iniSections
-      textList = foldl (\acc x -> op acc x) [] keysText
+      textList = foldl (\acc x -> appendRight acc x) [] keysText
       stringList = map unpack textList
   putStr $ intercalate "\n" stringList
 
 
-op acc x =
+appendRight acc x =
   case x of
       Left _ -> acc
       Right y -> acc ++ y
 
-run = do
+run preferedFiletype = do
   inputStr <- getContents
   ini <- parseConfig
   let inputLines = sanitizeInput $ Data.List.lines inputStr
-  putStr $  ast2String $ Ast.build (ini, sections ini) inputLines
+      iniSections = sections ini
+  if preferedFiletype == ""
+  then putStr $  ast2String $ Ast.build (ini, iniSections) inputLines
+  else putStr $  ast2String $ Ast.build (ini, (pack preferedFiletype) : iniSections) inputLines
 
 
 sanitizeInput :: [String] -> [String]
