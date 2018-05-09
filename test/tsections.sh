d646862b282fdf4f090f3473b21d0d55087094f3
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-05-09 21:22:00 +0200

    precedence of type

--- test/tsections.sh
+++ test/tsections.sh
@@ -10,11 +10,18 @@ export SUBS_CONFIG=/tmp/test-sections.ini
 cat > $SUBS_CONFIG <<EOF
 [random]
 myMail=myMail@gmail.com
+[javascript]
+cl=var %s = class {}
+[php]
+cl=class %s {}
 EOF
 
 
 subsCompare "random section name" "myMail" "myMail@gmail.com"
 
+subsCompare "selects php when type is passed" "cl foo" "class foo {}" -p 'php'
+
+subsCompare "selects javascript when type is passed " "cl foo" "var foo = class {}" -p 'javascript'
 
 test_done
 
